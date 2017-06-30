e9b9b0647542a75acad3abc5476bb7c11a02f552
MariBit <mb20210@gmail.com>
2017-06-30 11:07:33 +0300

    add delete button (unstable version)

--- JS/notes.js
+++ JS/notes.js
@@ -5,14 +5,14 @@ var notepad = [{title:"hello", name:"bye"}];
 function init(){
   console.log("Once when a lion, the king of the jungle, was asleep, a little mouse began running up and down on him. This soon awakened the lion, who placed his huge paw on the mouse, and opened his big jaws to swallow him. Pardon, O King! cried the little mouse. Forgive me this time. I shall never repeat it and I shall never forget your kindness. And who knows, I may be able to do you a good turn one of these days! The lion was so tickled by the idea of the mouse being able to help him that he lifted his paw and let him go. Sometime later, a few hunters captured the lion, and tied him to a tree. After that they went in search of a wagon, to take him to the zoo. Just then the little mouse happened to pass by. On seeing the lion’s plight, he ran up to him and gnawed away the ropes that bound him, the king of the jungle. Was I not right? said the little mouse, very happy to help the lion.");
 }
-init();
+
+
 var listNote=[
 	{title:" The Lion and the Mouse",name:"Once when a lion, the king of the jungle, was asleep, a little mouse began running up and down on him. This soon awakened the lion, who placed his huge paw on the mouse, and opened his big jaws to swallow him. Pardon, O King! cried the little mouse. Forgive me this time. I shall never repeat it and I shall never forget your kindness. And who knows, I may be able to do you a good turn one of these days! The lion was so tickled by the idea of the mouse being able to help him that he lifted his paw and let him go. Sometime later, a few hunters captured the lion, and tied him to a tree. After that they went in search of a wagon, to take him to the zoo. Just then the little mouse happened to pass by. On seeing the lion’s plight, he ran up to him and gnawed away the ropes that bound him, the king of the jungle. Was I not right? said the little mouse, very happy to help the lion."},
 	]
 
 var titleInput  = document.getElementById("title");
 var nameInput   = document.getElementById("name");
-
 var messageBox  = document.getElementById("display");
 
 function insert( ) {
@@ -25,6 +25,84 @@ notepad.push({title:titleInput.value, name:nameInput.value});
 console.log(notepad);
 }
 
+$(document).ready(
+    function(){
+
+      init();
+          /*  $("input[name=ListItem]").keyup(function(event){
+                      if(event.keyCode == 13){
+                        $("#button").click();
+                      }
+                  });
+
+                  $(document).on('dblclick','li', function(){
+                    $(this).toggleClass('strike').fadeOut('slow');
+                  });
+
+                  $('input').focus(function() {
+                    $(this).val('');
+                  });
+
+                    $('ol').sortable();*/
+        $('#button').on('click',addNote);
+        $('.deleteMe').on('click',deleteNote);
+  });
+
+    function addNote(){
+      var toAdd = $('input[name=title]').val();
+      var toAdd2 =$('input[name=name]').val();
+      console.log("I am the 1 loop");
+      var note= $('ul').append(
+        '<li class="new">' +
+          "<p>Title: "+ toAdd + " Note: "+ toAdd2 +'</p>'+
+          '<button class="deleteMe">'+"Delete"+'</button>'
+        +'</li>');
+      //'<p>'+"To delete double click"+'</p>'+
+      console.log("I am the  2 loop");
+
+    }
+
+    function deleteNote(){
+      var note = $(this)
+      console.log("class class");
+      $(this).remove();
+    }
+
+/*$(document).ready(function(){
+      $(".deleteMe").click(function(){
+         $(this).closest(".new").remove();
+      });
+});
+
+
+$(document).ready(function(){
+$(".delete").click(function(){
+    $("li").remove();
+  });
+});
+*/
+
+function myFunction() {
+document.getElementById("form").reset();
+}
+
+var importance = document.getElementById('myList');
+importance.onchange = runBackgroundChange;
+
+function runBackgroundChange(first){
+    var value = first.srcElement.options[first.srcElement.selectedIndex].value;
+    if (value != 1) {
+        document.getElementById('myList').style.backgroundColor="green";
+    } else {
+        document.getElementById('myList').style.backgroundColor="black";
+    };
+}
+
+
+
+
+
+/*
 function clearAndShow( ) {
 var display= document.getElementById("list")
 var checkbox = document.createElement('input');
@@ -35,9 +113,7 @@ var t1= document.createTextNode("Title: "+titleInput.value +",    \nNote: " +nam
 li.appendChild(checkbox);
 li.appendChild(t1);
 display.appendChild(li);
-
 }
-
 function delBoxes(){
     var ul = document.getElementById('list');
     var boxes = document.getElementsByClassName('check');
@@ -55,6 +131,32 @@ function delBoxes(){
     }
 
 }
-function myFunction() {
-document.getElementById("form").reset();
-}
+$(document).ready(function(){
+$(function() {
+    var counter = 0;
+    var toAdd = $('input[name=title]').val();
+    var toAdd2 =$('input[name=name]').val();
+
+    $('#button').click(function() {
+
+        var noteBox= $('ul').append('<li>' +"Title: "+ toAdd +" Note: \n"+toAdd2+ '</li>'),
+            removeButton = $('<input type="button" value="delete" id="removebtn' + (counter)+'"/>'),
+           containerLi = $("<li/>");
+
+            removeButton.click(function(e) {
+            e.preventDefault();
+            containerLi.remove();
+        });
+
+        containerDiv
+            .append(noteBox)
+            .append(removeButton);
+
+        counter++;
+
+        $("li").append(containerLi);
+    });
+  });
+});
+
+*/
